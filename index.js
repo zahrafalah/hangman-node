439858b3aa9923c7f47afae2c9e06eaeb3a16d8a
zahra falah <sepidehfallah2@gmail.com>
2018-04-13 17:57:24 -0700

    Fri

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+var wordCons = require("./word.js");
+var letterCons = require("./letter.js");
+var inquirer = require("inquirer");
+
+var wordsToGuess = ["Guess", "Random", "Yellow", "Infusion", "Piano", "Awe", "Javascript", "Readme", "Index", "Mysql", "package"];
+var randoIndex = Math.floor(Math.random() * wordsToGuess.length);
+var randoWord = wordsToGuess[randoIndex];
+
+
+
+var letterGuessed;
+
+
+var myWord = new wordCons.wordCons(randoWord);
+var maxGuesses = 10;
+
 
 
 
