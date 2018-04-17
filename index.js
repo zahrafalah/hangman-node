64a639ca71955e0abfd9c5b6c73d806b5f4fc9b0
zahra falah <sepidehfallah2@gmail.com>
2018-04-17 15:09:35 -0700

    last

--- index.js
+++ index.js
@@ -14,6 +14,42 @@ var letterGuessed;
 var myWord = new wordCons.wordCons(randoWord);
 var maxGuesses = 10;
 
+function takeAGuess(){
 
+    console.log(myWord.toString());
+    
+	if (myWord.guessesMade.length >= maxGuesses){
+		console.log('You have no more guesses. Game Over!.');
+	return; //Game over
+    }
+    
+	inquirer.prompt([{
+		name: 'letter',
+		type: 'text',
+        message: 'Enter a letter:',
+        
+		validate: function(str){
+			var regEx = new RegExp("^[a-zA-Z\s]{1,1}$");
+			return regEx.test(str);
+                }
+                
+        }]).then(function(letterInput){ //Game control
+            
+				var letter = letterInput.letter; 
+                myWord.findLetter(letter); //Check
+                
+					if(myWord.isComplete()){ 
+					console.log('Yes! It was ' + myWord.toString() + '!');
+					return; 
+                    }
+                    
+				console.log('-------------------\n');
+				console.log('You have ' + (maxGuesses - myWord.guessesMade.length) + ' guesses left.')
+				takeAGuess(); //Recursive call
+				}
+  );
+}
+
+takeAGuess();
 
 
